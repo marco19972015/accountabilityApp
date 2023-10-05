0a7932e96b5b0862b440760006f0d31396ccf483
Marco Torres <marcoantonio.torres@icloud.com>
2023-10-05 12:23:16 -0400

    Created goals and manageGoals modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+// import the goals module so we can use functionanilty 
+import { GoalsModule } from './goals/goals.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    GoalsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
