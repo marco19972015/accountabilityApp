93c58aa251866b0a21f3c90cc2f706b57f7122ed
Marco Torres <marcoantonio.torres@icloud.com>
2023-10-05 20:24:58 -0400

    Added OnChanges and SimpleChanges artifacts to enable us to detect the value of our input data binding

--- src/app/goals/goal-detail/goal-detail.component.ts
+++ src/app/goals/goal-detail/goal-detail.component.ts
@@ -1,13 +1,13 @@
 // adding the Input, Output, EventEmitter artifacts
-import { Component, Input, OnInit ,Output, EventEmitter, ViewEncapsulation } from '@angular/core';
+import { Component, Input, OnInit ,Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-goal-detail',
   templateUrl: './goal-detail.component.html',
   styleUrls: ['./goal-detail.component.css'],
-  encapsulation: ViewEncapsulation.None  // setting this to none allows our css to leak out into other components
+// encapsulation: ViewEncapsulation.None  // setting this to none allows our css to leak out into other components
 })
-export class GoalDetailComponent implements OnInit{
+export class GoalDetailComponent implements OnChanges{
   // Defining a goalName property that uses the Input decorator and initailized to an empty string
   @Input() goalName = '';
 
@@ -22,6 +22,18 @@ export class GoalDetailComponent implements OnInit{
     this.currentGoal.emit(this.goalName);
   }
 
+  // ngOnChanges is called when ng detects that the value of an input data binding has changed
+  ngOnChanges(changes: SimpleChanges): void {
+    const product = changes['goalName'];
+    // if there is no product change don't log anything, if there is log the old and new value
+    if (!product.isFirstChange()){
+      const oldValue = product.previousValue;
+      const newValue = product.currentValue;
+      console.log(`Product changed from ${oldValue} to ${newValue}`);
+    }
+  }
+
+  /* NOTE this used the OnInit artifact which whould need to be implemented in the export line
   // with ngOnInit we can see what is in the selectedGoal variable
   ngOnInit(): void {
     console.log(`goalName is ${this.goalName} in the ngOnInit`);
@@ -31,6 +43,8 @@ export class GoalDetailComponent implements OnInit{
   constructor() {  // We should keep constructors empty and devoid of logic other than seeting initial variables
     console.log(`goalName is ${this.goalName} in the constructor`);
   }
+  */
+
 
 
 
